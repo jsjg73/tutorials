0d5d2dcb6a9cedcb27721d1da8149d68b41da2c6
jsjg73 <kjs930703@naver.com>
2021-12-30 16:46:03 +0900

    [D003]: Using XML Configuration

---
+++ spring-hibernate/src/main/java/com/springhibernate/example/conf/HibernateXMLConf.java
@@ -0,0 +1,12 @@
+package com.springhibernate.example.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ImportResource({"classpath:hibernate5Configuration.xml"})
+public class HibernateXMLConf {
+
+}
