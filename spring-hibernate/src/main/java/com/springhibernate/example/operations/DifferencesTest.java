5e06a59eea360fc2b253215455b8cfc2552c5cb4
jsjg73 <kjs930703@naver.com>
2022-01-03 22:37:21 +0900

    [D003] learningtests: persist method throws an exception.
    persist 메소드에 detached 상태의 entity object를 전달하면 에러가
    발생한다.

--- spring-hibernate/src/main/java/com/springhibernate/example/operations/DifferencesTest.java
+++ spring-hibernate/src/main/java/com/springhibernate/example/operations/DifferencesTest.java
@@ -1,9 +1,14 @@
 package com.springhibernate.example.operations;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import javax.persistence.PersistenceException;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,13 +18,36 @@ import com.springhibernate.example.operations.model.Person;
 
 public class DifferencesTest {
 	
-	@Test
-	public void initiateHibernate() {
+	@Test(expected = PersistenceException.class)
+	public void failToPersistDetachedObject() {
 		ApplicationContext ac = new AnnotationConfigApplicationContext(HibernateConf.class);
 		SessionFactory factory = ac.getBean(SessionFactory.class);
 		Session session = factory.openSession();
-		Person person = session.find(Person.class, 1l);
-		
-		assertTrue("The Tartar Steppe".equals(person.getName()));
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			
+			Person person = new Person();
+			person.setName("jaesung");
+			
+			assertFalse(session.contains(person));
+			
+			session.persist(person);
+			
+			assertTrue(session.contains(person));
+			
+			session.evict(person);
+			
+			assertFalse(session.contains(person));
+			
+			session.persist(person);
+		}catch (HibernateException e) {
+			if(tx!=null)tx.rollback();
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		factory.close();
+//		assertTrue("The Tartar Steppe".equals(person.getName()));
 	}
 }
